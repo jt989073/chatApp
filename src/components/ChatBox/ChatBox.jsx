d7fc6b6ca7782548f233c449a67b1485e3d9ffa4
jt989073 <thompsonjames0120@gmail.com>
2024-08-22 08:34:12 -0500

    updated chatbox name display

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import assets from '../../assets/assets'
+import './ChatBox.css'
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className="chat-box">
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>James Thompson <img className='dot' src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} alt="" />
+      </div>
+    </div>
   )
 }
 
