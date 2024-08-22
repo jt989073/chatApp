475ad262a6b81218ab2addb164750d6036fe5669
jt989073 <thompsonjames0120@gmail.com>
2024-08-22 13:34:09 -0500

    added basic firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuP8xp6OawPR-UUK1UkPYhVyBczhrz50I",
+  authDomain: "chatapp-442b3.firebaseapp.com",
+  projectId: "chatapp-442b3",
+  storageBucket: "chatapp-442b3.appspot.com",
+  messagingSenderId: "768622212879",
+  appId: "1:768622212879:web:475044f2bb7161da33f07a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
